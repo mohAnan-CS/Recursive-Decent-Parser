e9500b8b6775472cd909586c02a8202a65790bfe
Mohammad Anan <smoker_shuger@hotmail.com>
2023-07-05 13:59:35 +0300

    Add non-terminal, terminal and reserved word files

---
+++ src/main/java/com/birzeit/recursivedescentparser/scanner/Token.java
@@ -0,0 +1,34 @@
+package com.birzeit.recursivedescentparser.scanner;
+
+public class Token {
+
+    private  String type;
+    private  String value;
+
+    public Token(String type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + type + ": " + value + "]";
+    }
+
+}
