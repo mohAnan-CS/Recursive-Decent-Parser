73048a355dc9c6784654bd284853acc58e5a6d41
Mohammad Anan <smoker_shuger@hotmail.com>
2023-07-07 22:53:29 +0300

    Make exceptions return line number

--- src/main/java/com/birzeit/recursivedescentparser/parser/GrammarParser.java
+++ src/main/java/com/birzeit/recursivedescentparser/parser/GrammarParser.java
@@ -12,6 +12,8 @@ public class GrammarParser {
     public static HashMap<String, String> terminals;
     public static HashMap<String, String> reservedWords;
     public static String currentToken;
+    public static char currentChar ;
+    public static int currentLine = 1;
 
     public static void main(String[] args) throws Exception {
 
@@ -39,9 +41,9 @@ public class GrammarParser {
                 "const\n" +
                 "  len=100;\n" +
                 "var\n" +
-                "  total:int;\n" +
+                "  num:int;\n" +
                 "  \n" +
-                "routine compute;\n" +
+                "routine loop;\n" +
                 "  \n" +
                 "  var\n" +
                 "    num:int;\n" +
@@ -62,9 +64,17 @@ public class GrammarParser {
     }
 
     private void projectDeclaration() throws Exception {
-        System.out.println("projectDeclaration");
+
+        //project-declaration  project-def     "."
+
+        System.out.println("enter projectDeclaration");
         projectDef();
-        //match(".");
+        getToken();
+        if (currentToken.equals(".")) {
+            System.out.println("input successfully parsed ...");
+        } else {
+            throw new Exception("Error at line " + currentLine + " parsing at token number " + current + " expected '.' but found " + currentToken);
+        }
 
 
     }
@@ -76,6 +86,7 @@ public class GrammarParser {
         projectHeading();
         declarations();
         compoundStatement();
+
     }
 
     private void declarations() throws Exception {
@@ -91,7 +102,6 @@ public class GrammarParser {
             System.out.println("finished constDeclaration");
             varDeclaration();
             System.out.println("finished varDeclaration");
-            //subroutine-decl
             subroutineDeclaration();
         }
 
@@ -112,7 +122,7 @@ public class GrammarParser {
         if (currentToken.equals(";")) {
             System.out.println("subroutine declaration valid");
         } else {
-            throw new Exception("Error parsing at token number " + current + " expected ';' but found " + currentToken);
+            throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected ';' but found " + currentToken);
         }
 
 
@@ -136,7 +146,7 @@ public class GrammarParser {
                 System.out.println("current token is " + currentToken);
 
             } else {
-                throw new Exception("Error parsing at token number " + current + " expected 'end' but found " + currentToken);
+                throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected 'end' but found " + currentToken);
             }
         } else {
             throw new Exception("Error parsing at token number " + current + " expected 'start' but found " + currentToken);
@@ -159,7 +169,7 @@ public class GrammarParser {
                     System.out.println("stmt list valid");
                     getToken();
                 } else {
-                    throw new Exception("Error parsing at token number " + current + " expected ';' but found " + currentToken);
+                    throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected ';' but found " + currentToken);
                 }
             }
         }
@@ -184,7 +194,7 @@ public class GrammarParser {
             if (!reservedWords.containsKey(currentToken)) {
                 assStmt();
             } else {
-                throw new Exception("Error parsing at token number " + current + " cant resolve statement name value as reserved word ");
+                throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " cant resolve statement name value as reserved word ");
             }
         }
 
@@ -214,13 +224,13 @@ public class GrammarParser {
                     if (currentToken.equals("do")){
                         statement();
                     }else{
-                        throw new Exception("Error parsing at token number " + current + " expected 'do' but found '" + currentToken+"'");
+                        throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected 'do' but found '" + currentToken+"'");
                     }
                 }else {
-                    throw new Exception("Error parsing at token number " + current + " expected ')' but found '" + currentToken+"'");
+                    throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected ')' but found '" + currentToken+"'");
                 }
             }else {
-                throw new Exception("Error parsing at token number " + current + " expected '(' but found '" + currentToken+"'");
+                throw new Exception("Error at line " + currentLine +  " : error parsing at token number " + current + " expected '(' but found '" + currentToken+"'");
             }
         }
 
@@ -245,13 +255,13 @@ public class GrammarParser {
                         if (currentToken.equals("endif")) {
                             System.out.println("valid if stmt");
                         } else {
-                            throw new Exception("Error parsing at token number " + current + " expected 'endif' but found " + currentToken);
+                            throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected 'endif' but found " + currentToken);
                         }
                     } else {
-                        throw new Exception("Error parsing at token number " + current + " expected 'then' but found " + currentToken);
+                        throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected 'then' but found " + currentToken);
                     }
                 } else {
-                    throw new Exception("Error parsing at token number " + current + " expected ')' but found " + currentToken);
+                    throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected ')' but found " + currentToken);
                 }
             }
         }
@@ -289,7 +299,7 @@ public class GrammarParser {
         if (currentToken.equals("=") || currentToken.equals("<>") || currentToken.equals("<") || currentToken.equals("<=") || currentToken.equals(">") || currentToken.equals(">=")) {
             System.out.println("relational operator is valid");
         } else {
-            throw new Exception("Error parsing at token number " + current + " expected relational operator but found " + currentToken);
+            throw new Exception("Error at line " + currentLine +  " : error parsing at token number " + current + " expected relational operator but found " + currentToken);
         }
 
     }
@@ -310,10 +320,10 @@ public class GrammarParser {
                     System.out.println("token after stmt name is ':='");
                     arthExp();
                 } else {
-                    throw new Exception("Error parsing at token number " + current + " expected is ':=' but found " + currentToken);
+                    throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected is ':=' but found " + currentToken);
                 }
             } else {
-                throw new Exception("Error parsing at token number " + current + " cant define ass-stmt name as reserved word");
+                throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " cant define ass-stmt name as reserved word");
             }
         } else {
             retrieveToken();
@@ -356,7 +366,22 @@ public class GrammarParser {
         //	term  factor    ( mul-sign       factor  )*
 
         System.out.println("enter term");
-        factor();
+        while (currentToken.equals("*") || currentToken.equals("/") || !currentToken.equals("%") ) {
+            factor();
+            mulSign();
+        }
+
+    }
+
+    private void mulSign() throws Exception{
+
+        // mul-sign  "*"    |  "/"  |  “%”
+        getToken();
+        if (currentToken.equals("*") || currentToken.equals("/") || currentToken.equals("%")) {
+            System.out.println("mul sign found");
+        } else {
+            throw new Exception("Error at line " + currentLine +  " : error parsing at token number " + current + " expected mul-sign but found " + currentToken);
+        }
 
     }
 
@@ -373,7 +398,7 @@ public class GrammarParser {
             if (currentToken.equals(")")) {
                 System.out.println("factor end with )");
             } else {
-                throw new Exception("Error parsing at token number " + current + " expected ')' but found " + currentToken);
+                throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected ')' but found " + currentToken);
             }
         } else {
             nameValue();
@@ -398,13 +423,13 @@ public class GrammarParser {
                     if (currentToken.equals(")")) {
                         System.out.println("input stmt valid");
                     } else {
-                        throw new Exception("Error parsing at token number " + current + " expected ')' but found " + currentToken);
+                        throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected ')' but found " + currentToken);
                     }
                 } else {
-                    throw new Exception("Error parsing at token number " + current + " input name cant be reserved word");
+                    throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " input name cant be reserved word");
                 }
             } else {
-                throw new Exception("Error parsing at token number " + current + " expected '(' but found '" + currentToken + "'");
+                throw new Exception("Error at line " + currentLine + " error parsing at token number " + current + " expected '(' but found '" + currentToken + "'");
             }
         } else if (currentToken.equals("output")) {
             System.out.println("else if current equal output");
@@ -416,11 +441,11 @@ public class GrammarParser {
                 if (currentToken.equals(")")) {
                     System.out.println("output stmt valid");
                 } else {
-                    throw new Exception("Error parsing at token number " + current + " expected ')' but found " + currentToken);
+                    throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected ')' but found " + currentToken);
                 }
 
             } else {
-                throw new Exception("Error parsing at token number " + current + " expected '(' but found '" + currentToken + "'");
+                throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " expected '(' but found '" + currentToken + "'");
             }
         }
 
@@ -443,10 +468,10 @@ public class GrammarParser {
                     System.out.println("token equals ';'");
                     //getToken();
                 } else {
-                    throw new Exception("Error parsing at " + current + "expected ';' but found " + currentToken);
+                    throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "expected ';' but found " + currentToken);
                 }
             } else {
-                throw new Exception("Error parsing at token number " + current + " routine name can be reserved word ");
+                throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " routine name can be reserved word ");
 
             }
         } else {
@@ -471,7 +496,7 @@ public class GrammarParser {
                     System.out.println("token equals ';' ");
                     getToken();
                 } else {
-                    throw new Exception("Error parsing at " + current + "expected ';' but found " + tokenList.get(current));
+                    throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "expected ';' but found " + tokenList.get(current));
                 }
             }
             System.out.println("end of while loop in varDeclaration");
@@ -490,11 +515,11 @@ public class GrammarParser {
         if (currentToken.equals(":")) {
             getToken();
             if (!currentToken.equals("int")) {
-                throw new Exception("Error parsing at " + current + "expected 'int' but found " + tokenList.get(current));
+                throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "expected 'int' but found " + tokenList.get(current));
             }
             System.out.println("end of varItem");
         } else {
-            throw new Exception("Error parsing at token number " + current + "expected ':' but found " + tokenList.get(current));
+            throw new Exception("Error at line " + currentLine +  " : error parsing at token number " + current + "expected ':' but found " + tokenList.get(current));
         }
     }
 
@@ -509,7 +534,7 @@ public class GrammarParser {
         } else if (!reservedWords.containsKey(currentToken)) {
             System.out.println("token equals name in nameValue");
         } else {
-            throw new Exception("Error parsing at " + current + "name value cant be a reserved word -> " + tokenList.get(current));
+            throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "name value cant be a reserved word -> " + tokenList.get(current));
         }
 
     }
@@ -528,11 +553,11 @@ public class GrammarParser {
                     System.out.println("token equals name in loop");
                     getToken();
                 } else {
-                    throw new Exception("Error parsing at " + current + "var name cant be a reserved word -> " + tokenList.get(current));
+                    throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "var name cant be a reserved word -> " + tokenList.get(current));
                 }
             }
         } else {
-            throw new Exception("Error parsing at " + current + "var name cant be a reserved word -> " + tokenList.get(current));
+            throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "var name cant be a reserved word -> " + tokenList.get(current));
         }
     }
 
@@ -555,7 +580,7 @@ public class GrammarParser {
                     System.out.println("token equals ';' ");
                     getToken();
                 } else {
-                    throw new Exception("Error parsing at " + current + "expected ';' but found " + tokenList.get(current));
+                    throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "expected ';' but found " + tokenList.get(current));
                 }
             }
         } else {
@@ -582,14 +607,14 @@ public class GrammarParser {
                     Integer.parseInt(currentToken);
                     System.out.println("token is integer value");
                 } catch (NumberFormatException e) {
-                    throw new Exception("Error parsing at " + current + "expected integer value but found " + currentToken);
+                    throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "expected integer value but found " + currentToken);
                 }
             } else {
                 System.out.println("Curren token in exception !! is  " + currentToken);
-                throw new Exception("Error parsing at token number " + current + " expected '=' but found " + currentToken);
+                throw new Exception("Error at line " + currentLine + " error parsing at token number " + current + " expected '=' but found " + currentToken);
             }
         } else {
-            throw new Exception("Error parsing at " + current + "const name cant be a reserved word -> " + currentToken);
+            throw new Exception("Error at line " + currentLine +  " : error parsing at " + current + "const name cant be a reserved word -> " + currentToken);
         }
 
 
@@ -610,13 +635,13 @@ public class GrammarParser {
                 if (currentToken.equals(";")) {
                     System.out.println("Project heading is correct");
                 } else {
-                    throw new Exception("Error parsing at " + current + "expected ; but found " + tokenList.get(current));
+                    throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "expected ; but found " + tokenList.get(current));
                 }
             } else {
-                throw new Exception("Error parsing at token number " + current + " project name cant be a reserved word ");
+                throw new Exception("Error at line " + currentLine + " : error parsing at token number " + current + " project name cant be a reserved word ");
             }
         } else {
-            throw new Exception("Error parsing at " + current + "expected project keyword but found " + tokenList.get(current));
+            throw new Exception("Error at line " + currentLine + " : error parsing at " + current + "expected project keyword but found " + tokenList.get(current));
         }
 
 
@@ -629,6 +654,10 @@ public class GrammarParser {
         }
         String token = tokenList.get(current);
         currentToken = token;
+
+        if (currentChar == '\n') {
+            currentLine++;
+        }
 
         System.out.println("getToken()  '" + token + "'" + " number of token is " + current);
         current++;
